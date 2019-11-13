e4631a4b983a8d49c88239233a3d709020399f71
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-11-13 11:29:42 +0000

    fix(riffraff): Call cdk directly, not through yarn

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,12 +2,16 @@
 
 set -ex
 
+
 npm install
 npm install -g @zeit/ncc
+npm install -g aws-cdk
+
 npm run build
+
 cd cdk
 npm install
 npm run compile
-
 cp -R ../dist ./
+
 npx @guardian/node-riffraff-artifact
