0540d46ac025c590dff1573ec957fbbcbc248910
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-11-13 12:03:49 +0000

    chore(riffraff): Point lambda to s3 bucket rather than from locally-generated asset

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,14 +1,34 @@
 import cdk = require("@aws-cdk/core");
 import lambda = require("@aws-cdk/aws-lambda");
+import s3 = require("@aws-cdk/aws-s3");
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const deployBucket = s3.Bucket.fromBucketName(
+      this,
+      "stack-switchboard",
+      "stack-switchboard"
+    );
+
+    const stackParameter = new cdk.CfnParameter(this, "stack", {
+      type: "String",
+      description: "Stack"
+    });
+
+    const stageParameter = new cdk.CfnParameter(this, "stage", {
+      type: "String",
+      description: "Stage"
+    });
+
     new lambda.Function(this, "stack-switchboard-dev", {
       handler: "index.handler",
       runtime: lambda.Runtime.NODEJS_10_X,
-      code: lambda.Code.fromAsset("../dist"),
+      code: lambda.Code.fromBucket(
+        deployBucket,
+        `${stackParameter.valueAsString}/${stageParameter.valueAsString}/backend/backend.zip`
+      ),
       description: ""
     });
   }
