e3e3dea0a45e164784183040515fbe9dbf009fa1
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-11-21 09:06:38 +0000

    feat: Get React app up and running

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from "./components/table"
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
