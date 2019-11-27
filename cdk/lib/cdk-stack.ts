f6088c136cec593ff33e8a4a83bdad309a744333
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-11-27 16:47:06 +0000

    fix: State API environment in description, not ID

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -45,12 +45,9 @@ export class CdkStack extends cdk.Stack {
 
     switchboardLambda.addToRolePolicy(statement);
 
-    new apigateway.LambdaRestApi(
-      this,
-      `stack-switchboard-api-${stageParameter.valueAsString}`,
-      {
-        handler: switchboardLambda
-      }
-    );
+    new apigateway.LambdaRestApi(this, "stack-switchboard-api", {
+      handler: switchboardLambda,
+      description: `API for stack-switchboard lambda in ${stackParameter.valueAsString} env`
+    });
   }
 }
